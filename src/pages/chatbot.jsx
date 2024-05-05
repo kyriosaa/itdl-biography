a07cf29762ac82ba5b92380c4eb5d5ff025fe873
kyriosaa <kyriosaaph@gmail.com>
2024-05-05 16:30:22 +0800

    Update

--- src/pages/chatbot.jsx
+++ src/pages/chatbot.jsx
@@ -1,11 +1,47 @@
 import React, { useState } from "react";
 import Navbar from "../NavBar";
 import { Link } from "react-router-dom";
-import Chatroom from "../chatgpt/chatroom";
-// import UserPic from "../images/user.jpg";
-// import ChatGptPic from "../images/chatgpt.jpg";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
 function Chatbot() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    setIsTyping(true);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+    setMessage("");
+
+    await openai.chat.completions
+      .create({
+        model: "gpt-4-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are a helpful assistant",
+          },
+          // Gives AI the history of the chats
+          ...chats,
+        ],
+      })
+      .then((result) => {
+        msgs.push(result.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div>
       <head>
@@ -30,9 +66,53 @@ function Chatbot() {
                   Final Project Demo
                 </Link>
               </header>
-
               {/* -- Content -- */}
-              <Chatroom />
+              <section>
+                <h1>AI Chatbot</h1>
+                <strong>
+                  This is a chatbot that you can use to ask questions! Try
+                  typing any question in and see what it says back!
+                </strong>
+                <br />
+                <br />
+                <br />
+                <div>
+                  <section>
+                    {chats && chats.length
+                      ? chats.map((chat, index) => (
+                          <p
+                            key={index}
+                            className={chat.role === "user" ? "user_msg" : ""}
+                          >
+                            <span>{chat.role}</span>
+                            <span>:</span>
+                            <span>{chat.content}</span>
+                          </p>
+                        ))
+                      : ""}
+                  </section>
+
+                  {isTyping ? (
+                    <div>
+                      <p>
+                        <i>Typing...</i>
+                      </p>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+
+                  <form onSubmit={(e) => chat(e, message)}>
+                    <input
+                      type="text"
+                      name="message"
+                      value={message}
+                      placeholder="Send a message"
+                      onChange={(e) => setMessage(e.target.value)}
+                    />
+                  </form>
+                </div>
+              </section>
             </div>
           </div>
           <Navbar />
